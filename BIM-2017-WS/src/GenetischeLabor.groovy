a23b0432091333164b7f17a4f469789782ba1bf0
ferlyloic <ferlyloic@yahoo.fr>
2017-11-07 12:35:37 +0100

    Last updates

--- BIM-2017-WS/src/GenetischeLabor.groovy
+++ BIM-2017-WS/src/GenetischeLabor.groovy
@@ -5,30 +5,30 @@ unreifeDNA_key = "Transkription DNA NR1H4 chromosome 12, 100867679:100957641"
 File file = new File( /..\files\NR1H4-BIM-Uebung.fasta/)
 fastaData = FastaConnector.readData(file)
 String unreifeDNAString = fastaData[unreifeDNA_key];
-String unreifeRNAString = DNA_Object.toRnaString(unreifeDNAString)
+String unreifeRNAString = Nucleotid_Object.toRnaString(unreifeDNAString)
 fastaData[unreifeDNA_key+" (RNA)"] = unreifeRNAString
 String reifeRNAString = ''
-for(int i = 1; i < 12;i++) {
-    fastaData["Exon $i (RNA)"] = DNA_Object.toRnaString(fastaData["Exon $i"])
+for(int i in 1 .. 11) {
+    fastaData["Exon $i (RNA)"] = Nucleotid_Object.toRnaString(fastaData["Exon $i"])
     reifeRNAString +=fastaData["Exon $i (RNA)"]
 }
 //println reifeRNAString
 fastaData +=["NR1H4-reife-mRNA":reifeRNAString]
 FastaConnector.writeData(fastaData,"NR1H4-reife-mRNA", "NR1H4-reife-mRNA")
-int starcodon = DNA_Object.getStartcodonIndex(reifeRNAString)
+int starcodon = Nucleotid_Object.getStartcodonIndex(reifeRNAString)
 println "reife RNA-length is ${reifeRNAString.length()}"
 println "reife RNA at startcodon ${reifeRNAString.substring(starcodon)}"
 println "first Startcodon-Index is = $starcodon"
-println "Stopcodon-Index is = ${DNA_Object.getStopcodonIndex(starcodon, reifeRNAString)}"
-String rnaSequenceToTranslate = DNA_Object.getSequenceToTranslate(reifeRNAString)
+println "Stopcodon-Index is = ${Nucleotid_Object.getStopcodonIndex(starcodon, reifeRNAString)}"
+String rnaSequenceToTranslate = Nucleotid_Object.getSequenceToTranslate(reifeRNAString)
 println"Translatable sequence is $rnaSequenceToTranslate"
-int firstExonIndex = DNA_Object.getIndex(unreifeRNAString, fastaData["Exon 1 (RNA)"])
-int secondExonIndex = DNA_Object.getIndex(unreifeRNAString, fastaData["Exon 2 (RNA)"])
+int firstExonIndex = Nucleotid_Object.getIndex(unreifeRNAString, fastaData["Exon 1 (RNA)"])
+int secondExonIndex = Nucleotid_Object.getIndex(unreifeRNAString, fastaData["Exon 2 (RNA)"])
 println "Exon-1-Startindex ist = $firstExonIndex"
 println "Exon-2-Startindex ist = $secondExonIndex"
-//String intron_1 = unreifeRNAString.substring(fastaData["Exon 1 (RNA)"].toString().length(), DNA_Object.getIndex(unreifeRNAString,fastaData["Exon 2 (RNA)"]))
-String firstIntron = DNA_Object.getFirstIntron(unreifeRNAString, fastaData["Exon 1 (RNA)"], fastaData["Exon 2 (RNA)"])
-String lastIntron = DNA_Object.getLastIntron(unreifeRNAString, fastaData["Exon 10 (RNA)"], fastaData["Exon 11 (RNA)"])
+//String intron_1 = unreifeRNAString.substring(fastaData["Exon 1 (RNA)"].toString().length(), Nucleotid_Object.getIndex(unreifeRNAString,fastaData["Exon 2 (RNA)"]))
+String firstIntron = Nucleotid_Object.getFirstIntron(unreifeRNAString, fastaData["Exon 1 (RNA)"], fastaData["Exon 2 (RNA)"])
+String lastIntron = Nucleotid_Object.getLastIntron(unreifeRNAString, fastaData["Exon 10 (RNA)"], fastaData["Exon 11 (RNA)"])
 println "erste Intron hat die Laenge ${firstIntron.length()} : \n$firstIntron"
 println "letzte Intron hat die Laenge ${lastIntron.length()} : \n$lastIntron"
 String firstIntronHeader = 'First Intron (RNA)'
@@ -37,6 +37,26 @@ String rnaSequenceToTranslateHeader = "RNA Sequence to translate"
 fastaData+=[(firstIntronHeader): firstIntron]
 fastaData+=[(lastIntronHeader): lastIntron]
 fastaData+=[(rnaSequenceToTranslateHeader): rnaSequenceToTranslate]
+String rnaVarianteTotranslate = ''
+for(i in 1 .. 11){
+    rnaVarianteTotranslate += fastaData["Exon $i (RNA)"]
+    if(i == 1){
+        rnaVarianteTotranslate += fastaData['First Intron (RNA)']
+    }
+}
+fastaData+= ["reife RNA Sequence-variante":rnaVarianteTotranslate]
+println rnaVarianteTotranslate
+rnaVarianteTotranslate = Nucleotid_Object.getSequenceToTranslate(rnaVarianteTotranslate)
+String rnaSequenceVarianteToTranslateHeader = "RNA Sequence-variante to translate"
+fastaData+=[(rnaSequenceVarianteToTranslateHeader): rnaVarianteTotranslate]
+String protein1 = Nucleotid_Object.rnaToProtein(rnaSequenceToTranslate)
+println"Protein ist $protein1"
+fastaData+=["NR1H4-Protein": protein1]
+FastaConnector.writeData(fastaData,"NR1H4-Protein","NR1H4-Protein")
+String protein2 = Nucleotid_Object.rnaToProtein(rnaVarianteTotranslate)
+println"Proteinvariante ist $protein2"
+fastaData+=["NR1H4-Protein-Variante": protein2]
+FastaConnector.writeData(fastaData,"NR1H4-Protein-Variante","NR1H4-Protein-Variante")
 FastaConnector.writeData(fastaData, "NR1H4-Introns", firstIntronHeader, lastIntronHeader)
 Set rnaSequences = fastaData.findAll {it.key.toString().contains('RNA')}.keySet()
 FastaConnector.writeData(fastaData,"NR1H4-BIM-Uebung (RNA)",rnaSequences.sort().toArray())
--- BIM-2017-WS/src/Nucleotid_Object.groovy
+++ BIM-2017-WS/src/Nucleotid_Object.groovy
@@ -1,4 +1,4 @@
-class DNA_Object {
+class Nucleotid_Object {
     static String toRnaString(String dna) {
         dna.replace('T','U')
     }
@@ -20,7 +20,9 @@ class DNA_Object {
     }
     static int getStartcodonIndex(String sequence){
         String startcodon = "AUG"
-        return getIndex(sequence,startcodon)
+        int index = getIndex(sequence,startcodon)
+        println "erste Startcodon ist ${index+1}"
+        return index
     }
     static int getStopcodonIndex(int startcodonIndex, String sequence){
         String stopcodon1 = "UAG"
@@ -34,6 +36,7 @@ class DNA_Object {
                 codon = sequence.substring(i,i+3)
                 if (codon == stopcodon1 || codon == stopcodon2 || codon == stopcodon3) {
                     println "[$codon]|"
+                    println "erste Stopcodon ist ${i+1}"
                     return i
                 } else {
                     print codon + "|"
@@ -60,5 +63,36 @@ class DNA_Object {
         int startcodon = getStartcodonIndex(rnaSequence)
         int stopcodonIndex = getStopcodonIndex(startcodon,rnaSequence)
         rnaSequence.substring(startcodon,stopcodonIndex+3)
+    }
+    static String rnaToProtein(String rna){
+        def map = ["UUU":"F", "UUC":"F", "UUA":"L", "UUG":"L",
+            "UCU":"S", "UCC":"S", "UCA":"S", "UCG":"S",
+            "UAU":"Y", "UAC":"Y", "UAA":"*", "UAG":"*",
+            "UGU":"C", "UGC":"C", "UGA":"*", "UGG":"W",
+            "CUU":"L", "CUC":"L", "CUA":"L", "CUG":"L",
+            "CCU":"P", "CCC":"P", "CCA":"P", "CCG":"P",
+            "CAU":"H", "CAC":"H", "CAA":"Q", "CAG":"Q",
+            "CGU":"R", "CGC":"R", "CGA":"R", "CGG":"R",
+            "AUU":"I", "AUC":"I", "AUA":"I", "AUG":"M",
+            "ACU":"T", "ACC":"T", "ACA":"T", "ACG":"T",
+            "AAU":"N", "AAC":"N", "AAA":"K", "AAG":"K",
+            "AGU":"S", "AGC":"S", "AGA":"R", "AGG":"R",
+            "GUU":"V", "GUC":"V", "GUA":"V", "GUG":"V",
+            "GCU":"A", "GCC":"A", "GCA":"A", "GCG":"A",
+            "GAU":"D", "GAC":"D", "GAA":"E", "GAG":"E",
+            "GGU":"G", "GGC":"G", "GGA":"G", "GGG":"G"]
+        String result = ''
+        String codon = ''
+        int j = rna.length() % 3
+        if (j == 0){
+            for(int i = 0; i < rna.length(); i+=3){
+                codon = rna.substring(i, i+3)
+               // println(codon)
+                // println(map[codon])
+                result +=map[codon]
+            }
+        }
+
+        return result
     }
 }
